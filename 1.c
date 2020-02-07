0d8d6a938d3b4bf18423f3ce585b553e0b03334b
John Smith <buicomauxanh@gmail.com>
2020-02-08 02:50:48 +0700

    bug

---
+++ 1.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+int main(){
+    printf("Hello World");
+    getchar();
+    return 0;
+}
+#include<stdio.h>
+
+int main()
+{
+	int x,S = 0 ;
+   	do 
+   	{
+       	 scanf("%d",&x);
+       	 if (x != 0) S = S + x; 
+   	}
+   while  (x!=0);
+	printf("Sum: %d",S);
+	return 0;
+}
---
+++ 2.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <conio.h>
+#include <ctype.h>
+#include <string.h>
+int main()
+{
+    int nVowels, nConsonants, nOthers;
+    char ch[] = "mlem kamehameha mlem";
+    int len = strlen(ch);
+    for (int i = 0; i <= len; i++)
+    {
+        int c = toupper(ch[i]);
+        if (c >= 'A' && c <= 'Z')
+        {
+            switch (c)
+            {
+            case 'A':
+            case 'E':
+            case 'I':
+            case 'O':
+            case 'U':
+            {
+                nVowels++;
+                break;
+            }
+            default:
+                nConsonants++;
+            }
+        }
+        else
+            nOthers++;
+    }
+    printf("\nprint out nVowels: %d", nVowels);
+    printf("\nprint out nConsonants: %d", nConsonants);
+    printf("\nPrint out nOthers: %d", nOthers - 1 );
+    getch();
+    return 0;
+}
