63576e0794123f53b811ae3d97c9e684368b8d7a
John Smith <buicomauxanh@gmail.com>
2020-02-15 01:34:18 +0700

    1

--- c/1.c
+++ c/1.c
@@ -1,14 +1,18 @@
-#include<stdio.h>
-
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+#include<stdlib.h>
+#include<conio.h>
 int main()
 {
-	int x,S = 0 ;
-   	do 
-   	{
-       	 scanf("%d",&x);
-       	 if (x != 0) S = S + x; 
-   	}
-   while  (x!=0);
-	printf("Sum: %f",S);
+	int x, S = 0;
+	do
+	{
+		scanf("%d", &x);
+		if (x != 0)
+			S = S + x;
+	} while (x != 0);
+	printf("Sum: %f", S);//-----------------------------------------------
+	getch();
 	return 0;
 }
